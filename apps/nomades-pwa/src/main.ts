6e9c8bb4d60383da8040e551c4ecc44cd697e061
FazioNico <contact@nicolasfazio.ch>
2020-02-20 13:01:41 +0100

    upd(capacitor): add native plugin accesor

--- apps/nomades-pwa/src/main.ts
+++ apps/nomades-pwa/src/main.ts
@@ -1,5 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+import "@ionic/pwa-elements";
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -12,5 +14,6 @@ platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .then(_=> {
     console.log('Nomades Network Version: ', environment.version);
+    defineCustomElements(window);
   })
   .catch(err => console.error(err));
