a157dc5f23ffb2033f9723bd950950d17d2a47e3
FazioNico <contact@nicolasfazio.ch>
2020-02-20 10:36:53 +0100

    fix(libs): dev api endpoint

--- libs/core/environments/environment.ts
+++ libs/core/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   authToken: 'authToken',
   // apiEndpoint: 'https://nomades-network.herokuapp.com/api',
-  apiEndpoint: 'http://localhost:3333/api',
+  apiEndpoint: 'http://localhost:3333',
 };
