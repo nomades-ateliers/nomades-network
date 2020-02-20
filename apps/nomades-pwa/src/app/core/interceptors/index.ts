41d2496f3bf98b3e214fa672918f2e380f0db58e
FazioNico <contact@nicolasfazio.ch>
2020-02-20 10:37:30 +0100

    upd(pwa): ad error handling logic with display method

--- apps/nomades-pwa/src/app/core/interceptors/index.ts
+++ apps/nomades-pwa/src/app/core/interceptors/index.ts
@@ -1,8 +1,15 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 // libs
 import { TokenInterceptor } from '@nomades-network/core/interceptors/token.interceptor';
+// app
+import { ErrorInterceptor } from './error.interceptor';
 
 export const INTERCEPTORS = [
+  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: ErrorInterceptor,
+    multi: true
+  },
   {
     provide: HTTP_INTERCEPTORS,
     useClass: TokenInterceptor,
