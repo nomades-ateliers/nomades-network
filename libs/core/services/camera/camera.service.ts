9c1b9acacd29247864eb2fd3408250ecf49e9039
FazioNico <contact@nicolasfazio.ch>
2020-02-27 12:43:31 +0100

    upd(libs): return correct key and add request perm

--- libs/core/services/camera/camera.service.ts
+++ libs/core/services/camera/camera.service.ts
@@ -7,7 +7,8 @@ const { Camera } = Plugins;
 export class CameraService {
 
   async takePicture(mode: 'Base64' | 'Uri' = 'Base64', quality = 90, allowEditing = true) {
-    // const perm = await Camera.requestPermissions().catch(err => err);
+    const perm = await Camera.requestPermissions()// .catch(err => err);
+    console.log('perm result: ', perm.results);
     const image = await Camera.getPhoto({
       quality,
       allowEditing,
@@ -19,7 +20,7 @@ export class CameraService {
     // You can access the original file using image.path, which can be 
     // passed to the Filesystem API to read the raw data of the image, 
     // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
-    const imageUrl = image[mode];
+    const imageUrl = image[mode.toLowerCase() + 'String'];
     // Can be set to the src of an image now
     return imageUrl;
   }
