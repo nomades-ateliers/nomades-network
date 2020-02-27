03a43f6f0560a269bab8cec7b1b51ddec00c15b3
FazioNico <contact@nicolasfazio.ch>
2020-02-27 11:54:49 +0100

    fix(libs): native - debug camera api service

--- libs/core/services/camera/camera.service.ts
+++ libs/core/services/camera/camera.service.ts
@@ -7,19 +7,19 @@ const { Camera } = Plugins;
 export class CameraService {
 
   async takePicture(mode: 'Base64' | 'Uri' = 'Base64', quality = 90, allowEditing = true) {
-    const perm = await Camera.requestPermissions().catch(err => err);
+    // const perm = await Camera.requestPermissions().catch(err => err);
     const image = await Camera.getPhoto({
       quality,
       allowEditing,
       resultType: CameraResultType[mode],
       source: CameraSource.Camera
     })// .catch(err => err) // do no catch error to send to main Error Handler
-    console.log('camera service: ', perm, image);
+    console.log('camera service: ', image);
     // image.webPath will contain a path that can be set as an image src. 
     // You can access the original file using image.path, which can be 
     // passed to the Filesystem API to read the raw data of the image, 
     // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
-    const imageUrl = image.webPath;
+    const imageUrl = image[mode];
     // Can be set to the src of an image now
     return imageUrl;
   }
