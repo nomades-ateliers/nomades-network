97a37e1e7e040a56a52573910508275a8f453dd2
FazioNico <contact@nicolasfazio.ch>
2020-03-05 12:18:00 +0100

    fix(api): use new mongoose connect logic

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -31,20 +31,33 @@ const PROTECTED_ROUTES: RouteInfo[] = [
     path: '/users/:id',
     method: RequestMethod.GET
   }
-]
+];
+
+const mongooseOptions: MongooseModuleOptions = {
+  
+  // fix mongoose deprecated warning
+  // doc: https://mongoosejs.com/docs/deprecations.html#-findandmodify-
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+};
+
 @Module({
   imports: [
-    MongooseModule.forRootAsync({
-      useFactory: () => {
-        console.log('check db url connection: -------', environment.getDBHost());
-        return <MongooseModuleOptions>{
-          uri: environment.getDBHost(),
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-          useCreateIndex: true,
-        }
-      },
-    }),
+
+    MongooseModule.forRoot(environment.getDBHost(), mongooseOptions),
+    // MongooseModule.forRootAsync({
+    //   useFactory: () => {
+    //     console.log('check db url connection: -------', environment.getDBHost());
+    //     return <MongooseModuleOptions>{
+    //       uri: environment.getDBHost(),
+    //       useNewUrlParser: true,
+    //       useUnifiedTopology: true,
+    //       useCreateIndex: true,
+    //     }
+    //   },
+    // }),
     MailerModule.forRootAsync({
       useFactory: () => ({
         transport: `smtps://${environment.mailer.user}:${environment.mailer.pass}@${environment.mailer.host}`,
